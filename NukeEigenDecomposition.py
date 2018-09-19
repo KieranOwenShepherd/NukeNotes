95f6553df23bc453746bbcdd54ac4045a3550aa0
KieranOwenShepherd <kieran.shepherd.horatio@gmail.com>
2018-09-19 11:54:35 +0930

    Update NukeEigenDecomposition.py

--- NukeEigenDecomposition.py
+++ NukeEigenDecomposition.py
@@ -5,6 +5,7 @@ except ImportError:
     sys.path.insert(0, "C:\Program Files\Side Effects Software\Houdini 16.0.671\python27\lib\site-packages")
     import numpy as np
 
+import numpy.linalg as la
 
 def sample_box(node, x_bounds, y_bounds, pixel_step, frame = None):
   if not frame:
@@ -18,6 +19,7 @@ def sample_box(node, x_bounds, y_bounds, pixel_step, frame = None):
       return
     t.setProgress(int(y/int(y_bounds[1])*100))
 
+
     samples = dict()
 
     #build dictionary (just convenient if we want to look up positions later) of samples
@@ -29,4 +31,22 @@ def sample_box(node, x_bounds, y_bounds, pixel_step, frame = None):
 
 box = nuke.selectedNode()['bbox']
 samples = sample_box(  nuke.selectedNode(), (box.x() , box.r()) , (box.y() , box.t()), 1)
+
+sample_array = np.array([v for n,v in samples.items()]).transpose()
+
+print np.mean(sample_array, axis=0)
+
+M = np.cov(sample_array)
+
+print sample_array.shape
+
+e, v = la.eig(M)
+idx = np.argsort(e)[::-1]
+e = e[idx]
+e = np.real_if_close(e)
+v = v[:, idx]
+
+print M
+print e
+print v
 
