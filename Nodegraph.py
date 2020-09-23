c6c92d51553392ff0e780a14eac5d8de3bba18bc
KieranOwenShepherd <kieran.shepherd.horatio@gmail.com>
2020-09-23 12:52:57 +0930

    Create Nodegraph.py

---
+++ Nodegraph.py
@@ -0,0 +1,9 @@
+import nuke
+from itertools import repeat, islice, tee
+
+def pairwise(iterable):
+  "s -> (s0,s1), (s1,s2), (s2,s3), ..."
+  a,b = tee(iterable)
+  next(b,None)
+  return zip(a,b)
+ 
